a8a24d84e312b15f8b3b2452556ed11d0d694abc
prabhakar <prabhakar@DESKTOP-BSEFP4L>
2019-07-11 17:46:06 +0100

    First commit

---
+++ Hello world/src/myfirstclass.java
@@ -0,0 +1,11 @@
+
+public class myfirstclass {
+	public static void main (String[] args)
+	{
+	System.out.println("Hello world ");
+	System.out.println("Iam Chitti! ");
+	
+	System.out.print("Hello world ");
+	System.out.print("Iam Chitti");
+    }
+                          }
